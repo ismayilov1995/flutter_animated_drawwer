a0dae633800e1730ec6ff5a827237a62a7c90754
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-22 18:14:02 +0400

    Disabled device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'menu_dashboard.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Flutter Animation Drawer",
-    theme: ThemeData(primarySwatch: Colors.blue),
-    home: MenuDashboard(),
-    debugShowCheckedModeBanner: false,
-  ));
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
+    runApp(MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Animation Drawer",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MenuDashboard(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
